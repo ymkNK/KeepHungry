327ca7ccd0549bd38f95b004c7a4f464e355ce2d
杨茗凯 <yangmingkai@shuidihuzhu.com>
2020-04-27 18:30:44 +0800

    add the lombok

---
+++ src/main/java/ReverseLinkedList/ListNode.java
@@ -0,0 +1,13 @@
+package main.java.ReverseLinkedList;
+
+/**
+ * Created by Yangmingkai on 2020-04-27.
+ */
+@Data
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
--- src/main/java/ReverseLinkedList/ReverseLinkedList.java
+++ src/main/java/ReverseLinkedList/ReverseLinkedList.java
@@ -1,9 +1,18 @@
 package ReverseLinkedList;
 
+import main.java.ReverseLinkedList.ListNode;
+
 /**
 * Created by ymkNK on 2020-04-27.
 */
 public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        var current = head;
+        while ()
+        return null;
+    }
+
 
     public static void main(String[] args) {
 
