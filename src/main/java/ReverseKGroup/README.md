7a6146dfbdc6ff5a716e8dca105eddea3f2eb967
杨茗凯 <ymknkcs@163.com>
2020-12-21 00:03:56 +0800

    merge

---
+++ src/main/java/ReverseKGroup/README.md
@@ -0,0 +1,7 @@
+# 2020-06-25 ReverseKGroup
+[地址](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/)
+
+## 原理
+
+
+## 结论
---
+++ src/main/java/ReverseKGroup/ReverseKGroup.java
@@ -0,0 +1,36 @@
+package ReverseKGroup;
+
+/**
+ * Created by ymkNK on 2020-06-25.
+ */
+public class ReverseKGroup {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode iter = head;
+        int count = 0;
+        while (head.next != null) {
+
+        }
+
+        return head;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode pre = head, next = pre.next;
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
