724d726c908cbe6f2409657edaee5c75c097530a
杨茗凯 <yangmingkai@shuidihuzhu.com>
2021-04-27 21:35:36 +0800

    add the test function by reflect

--- src/main/java/JVM07HowReflect/HowJvmReflect.java
+++ src/main/java/JVM07HowReflect/HowJvmReflect.java
@@ -3,18 +3,25 @@ package JVM07HowReflect;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * Created by ymkNK on 2021-04-26.
  */
 public class HowJvmReflect {
 
+    public static String PREFIX = "testcase";
+    public static String EXIT = "exit";
+
+    public static void testcase() {
+        System.out.println("Can't find the function.This is a default test case");
+    }
 
     /**
      * Created by ymk.
      * 1. 举例来说，我们可以通过 Class 对象枚举该类中的所有方法，我们还可以通过 Method.setAccessible（位于 java.lang.reflect 包，该方法继承自 AccessibleObject）绕过 Java 语言的访问权限，在私有方法所在类之外的地方调用该方法。
      **/
-    public static void case1() throws IllegalAccessException, InvocationTargetException {
+    public static void testcase1() throws IllegalAccessException, InvocationTargetException {
         System.out.println("===case 1===");
         Test test = new Test();
 //        test.testPrivate();
@@ -37,7 +44,7 @@ public class HowJvmReflect {
      * Created by ymk.
      * 反射耗时的例子
      **/
-    public static void case2() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    public static void testcase2() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         System.out.println("===case 2===");
         Test test = new Test();
         int invokeTimes = 100000;
@@ -60,9 +67,44 @@ public class HowJvmReflect {
         System.out.println("反射调用耗时:" + privateConsumeTime + "ms");
     }
 
+    public static void showEmptyLine() {
+        System.out.println("\n");
+    }
+    public static void showMenu() {
+        System.out.println("===Please select a function to test===\n" +
+                        "Type the number of test.\n" +
+                        "Type 'exit' to exit.");
+    }
+
+    public static void showExit() {
+        System.out.println("===Exit bye bye===");
+    }
+
+    public static Map<String, Method> getAllFunction(Class<?> aClass) {
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        HashMap<String, Method> functionMap = new HashMap<>();
+        for (Method method : declaredMethods) {
+            functionMap.put(method.getName(), method);
+        }
+        return functionMap;
+    }
 
     public static void main(String[] args) throws Exception {
-        case1();
-        case2();
+        showMenu();
+        Scanner scan = new Scanner(System.in);
+        Map<String, Method> allFunction = getAllFunction(HowJvmReflect.class);
+        while (scan.hasNext()) {
+            String next = scan.next();
+            next = next == null ? "" : next;
+            if (EXIT.equals(next.toLowerCase())) {
+                break;
+            }
+            String key = PREFIX + next;
+            allFunction.getOrDefault(key, allFunction.get(PREFIX)).invoke(null);
+            showEmptyLine();
+            showMenu();
+        }
+        showExit();
+        scan.close();
     }
 }
